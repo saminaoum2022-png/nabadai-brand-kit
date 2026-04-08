45eec86f330b2b38c70d133d89bdd3d26da0cfe0
saminaoum2022-png <saminaoum2022@gmail.com>
2026-04-08 20:04:22 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,10 +6,6 @@ const nextConfig = {
         source: "/(.*)",
         headers: [
           {
-            key: "X-Frame-Options",
-            value: "ALLOWALL",
-          },
-          {
             key: "Content-Security-Policy",
             value: "frame-ancestors *",
           },
